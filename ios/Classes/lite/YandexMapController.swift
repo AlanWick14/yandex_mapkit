a4887a0765a75310eedc340bccb1ecabf6f2c890
AlanWick14 <anvaralimjanovich@gmail.com>
2025-12-23 18:25:05 +0500

    last

--- ios/Classes/lite/YandexMapController.swift
+++ ios/Classes/lite/YandexMapController.swift
@@ -48,13 +48,6 @@ public class YandexMapController:
       print("⚠️ Adjusted frame to \(safeFrame)")
     }
 
-    let b = Bundle.main
-    print("BUNDLE ID:", b.bundleIdentifier as Any)
-    print("VERSION:", b.object(forInfoDictionaryKey: "CFBundleShortVersionString") as Any)
-    print("BUILD:", b.object(forInfoDictionaryKey: "CFBundleVersion") as Any)
-    print("NAME:", b.object(forInfoDictionaryKey: "CFBundleDisplayName") as Any)
-    print("EXE:", b.object(forInfoDictionaryKey: "CFBundleExecutable") as Any)
-
   
   
   // ✅ USE safeFrame, not frame
@@ -64,13 +57,6 @@ public class YandexMapController:
       name: "yandex_mapkit/yandex_map_\(id)",
       binaryMessenger: registrar.messenger()
     )
-
-    let b = Bundle.main
-    print("BUNDLE ID:", b.bundleIdentifier as Any)
-    print("VERSION:", b.object(forInfoDictionaryKey: "CFBundleShortVersionString") as Any)
-    print("BUILD:", b.object(forInfoDictionaryKey: "CFBundleVersion") as Any)
-    print("NAME:", b.object(forInfoDictionaryKey: "CFBundleDisplayName") as Any)
-    print("EXE:", b.object(forInfoDictionaryKey: "CFBundleExecutable") as Any)
 
   
   // MapKit should already be initialized from AppDelegate
